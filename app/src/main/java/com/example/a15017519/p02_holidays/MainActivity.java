f61e2dda70c15225ae132e10e6b7be61c0a35223
15017519 <15017519@myrp.edu.sg>
2017-04-28 14:35:43 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a15017519/p02_holidays/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.a15017519.p02_holidays;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView lv;
+        TextView tvYear;
+    }
+}
