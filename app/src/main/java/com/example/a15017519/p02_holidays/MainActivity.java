b312564aaaa089a4bd4450138fa1f87e326f3ccf
15017519 <15017519@myrp.edu.sg>
2017-04-28 14:39:23 +0800

    first screen

--- app/src/main/java/com/example/a15017519/p02_holidays/MainActivity.java
+++ app/src/main/java/com/example/a15017519/p02_holidays/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.a15017519.p02_holidays;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +17,19 @@ public class MainActivity extends AppCompatActivity {
 
         ListView lv;
         TextView tvYear;
+        ArrayList al;
+        ArrayAdapter aa;
+
+        lv = (ListView)findViewById(R.id.lv);
+
+        // adding new data into AL
+        al = new ArrayList<String>();
+        al.add("Secular");
+        al.add("Ethnic & Religion");
+
+        aa = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1,al);
+
+        lv.setAdapter(aa);
     }
 }
